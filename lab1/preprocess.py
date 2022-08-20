a5b638a9c1de70761ff654012a59a281a77517cf
PC-202107271659\Administrator <1030587461@qq.com>
2022-08-20 22:37:39 +0800

    BOW

--- lab1/preprocess.py
+++ lab1/preprocess.py
@@ -1,1 +1,32 @@
 import pandas as pd
+import numpy as np
+
+
+class BOW:
+    def __init__(self, train_path):
+        df = pd.read_csv(train_path, sep='\t')
+        self.corpus = df['Phrase']
+        self.vocab = []
+        self.idx = dict()
+        for phrase in self.corpus:
+            self.vocab = list(set(self.vocab + phrase.lower().split(' ')))
+        for i, voc in enumerate(self.vocab):
+            self.idx[voc] = i
+
+    def __getitem__(self, item):
+        return self.vocab[item]
+
+    def voc2idx(self, voc):
+        return self.idx.get(voc, -1)
+
+    def generate_bag(self, phrase):
+        bag = np.array([0] * len(self.vocab), dtype=np.float32)
+        words = phrase.split()
+        for word in words:
+            word_idx = self.voc2idx(word)
+            if word_idx != -1 and bag[word_idx] == 0:
+                bag[word_idx] = float(words.count(word))
+        return bag
+
+
+

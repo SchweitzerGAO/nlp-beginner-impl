b7b3cc9857547b893ef8cf7fee117ec6424e8fa9
PC-202107271659\Administrator <1030587461@qq.com>
2022-08-21 23:43:57 +0800

    prepared model, to do losses

--- lab1/model.py
+++ lab1/model.py
@@ -2,14 +2,17 @@ import numpy as np
 from preprocess import BOW, dataloader_bow
 
 
-
 class ScratchTextClassifier:
-    def __init__(self, bow,num_hidden=256):
-        self.len_vocab = len(bow.vocab)
-        self.num_cls = bow.num_cls
-
-
-
+    def __init__(self, len_vocab, num_cls, loss='ce', num_hidden=256):
+        self.len_vocab = len_vocab
+        self.num_cls = num_cls
+        self.num_hidden = num_hidden
+        self.weights = [np.random.randn(self.len_vocab, self.num_hidden),
+                        np.random.randn(self.num_hidden, self.num_cls)]
+        self.biases = [np.zeros(1, self.num_hidden), np.zeros(1, self.num_cls)]
+        self.output = []
+        self.activated_output = []
+        self.loss = loss
 
     def __call__(self, X):
         return self.forward(X)

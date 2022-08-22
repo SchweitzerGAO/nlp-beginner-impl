0a5a946be3014ef1d250b358601662d37165df5c
PC-202107271659\Administrator <1030587461@qq.com>
2022-08-22 17:46:03 +0800

    todo: backward function

--- lab1/model.py
+++ lab1/model.py
@@ -1,6 +1,21 @@
 import numpy as np
-from preprocess import BOW, dataloader_bow
 
+
+# softmax
+def softmax(x):
+    x_exp = np.exp(x)
+    x_exp_sum = np.sum(x_exp, axis=-1, keepdims=True)
+    return x_exp / x_exp_sum
+
+
+# cross entropy
+def cross_entropy(y, y_hat):  # y is gt and y_hat is prediction
+    return -np.sum(y * np.log(y_hat), axis=-1)
+
+
+# relu
+def relu(x):
+    return np.max(0, x)
 
 
 class ScratchTextClassifier:
@@ -18,4 +33,19 @@ class ScratchTextClassifier:
         return self.forward(X)
 
     def forward(self, X):
-        return None
+        for W, b in zip(self.weights, self.biases):
+            X = X @ W + b
+        X = softmax(X)
+        self.output.append(X)
+        activated = relu(X)
+        self.activated_output.append(activated)
+        return activated
+
+    def backward(self,y):
+        dw = []
+        db = []
+        return dw, db
+
+    def update_params(self, lr, dw, db):
+        self.weights = [W - lr * grad_w for W, grad_w in zip(self.weights, dw)]
+        self.biases = [b - lr * grad_b for b, grad_b in zip(self.biases, db)]

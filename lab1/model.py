df971a57f43229b645bf590db665ad09939fd8a4
PC-202107271659\Administrator <1030587461@qq.com>
2022-08-22 23:41:57 +0800

    todo: backward function

--- lab1/model.py
+++ lab1/model.py
@@ -1,4 +1,5 @@
 import numpy as np
+from preprocess import BOW, train_test_split, dataloader_bow
 
 
 # softmax
@@ -10,7 +11,7 @@ def softmax(x):
 
 # cross entropy
 def cross_entropy(y, y_hat):  # y is gt and y_hat is prediction
-    return -np.sum(y * np.log(y_hat), axis=-1)
+    return -np.sum(y * np.log(y_hat), axis=-1), y_hat - y  # the second one is the derivative of softmax
 
 
 # relu
@@ -18,6 +19,7 @@ def relu(x):
     return np.max(0, x)
 
 
+# an MLP classifier
 class ScratchTextClassifier:
     def __init__(self, len_vocab, num_cls, num_hidden=256):
         self.len_vocab = len_vocab
@@ -26,24 +28,27 @@ class ScratchTextClassifier:
         self.weights = [np.random.randn(self.len_vocab, self.num_hidden),
                         np.random.randn(self.num_hidden, self.num_cls)]
         self.biases = [np.zeros(1, self.num_hidden), np.zeros(1, self.num_cls)]
-        self.output = []
-        self.activated_output = []
+        self.P = []
+        self.X = []
 
-    def __call__(self, X):
-        return self.forward(X)
+    def __call__(self, x):
+        return self.forward(x)
 
-    def forward(self, X):
-        for W, b in zip(self.weights, self.biases):
-            X = X @ W + b
-        X = softmax(X)
-        self.output.append(X)
-        activated = relu(X)
-        self.activated_output.append(activated)
-        return activated
+    def forward(self, x):
+        for layer_idx, W, b in enumerate(zip(self.weights, self.biases)):
+            p = x @ W + b
+            self.P.append(p)
+            if layer_idx < len(self.weights) - 1:
+                x = relu(p)
+            else:
+                x = softmax(p)
+            self.X.append(x)
+        return self.X[-1]
 
     def backward(self, y):
-        dw = []
-        db = []
+        dw = [np.zeros(w.shape) for w in self.weights]
+        db = [np.zeros(b.shape) for b in self.biases]
+
         return dw, db
 
     def update_params(self, lr, dw, db):

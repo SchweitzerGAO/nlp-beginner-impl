ad9cda5f6be61753a6fd583d3feca922aab9b9e0
PC-202107271659\Administrator <1030587461@qq.com>
2023-03-12 20:46:52 +0800

    lab4 preprocess really finished

--- lab4/preprocess.py
+++ lab4/preprocess.py
@@ -1,9 +1,9 @@
 import torch
-from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
+from torch.nn.utils.rnn import pad_sequence
 import torch.utils.data.dataset as Dataset
 from torch.utils.data.dataloader import DataLoader
 import pickle as pkl
-from public.misc import Vocab
+from public.misc import Vocab, truncate_pad
 
 '''
 extract useful data from the original dataset
@@ -75,20 +75,21 @@ make the data digitalized
 
 
 def collate_fn(data):
-    sentences, labels = [d[0] for d in data], [d[1] for d in data]
-
+    chars, sentences, labels = [d[0] for d in data], [d[1] for d in data], [d[2] for d in data]
     sentences.sort(key=lambda x: len(x), reverse=True)
     labels.sort(key=lambda x: len(x), reverse=True)
+    chars.sort(key=lambda x: len(x), reverse=True)
 
-    lengths_sentence = torch.tensor([s.size(0) for s in sentences])
+    # lengths_sentence = torch.tensor([s.size(0) for s in sentences])
 
-    sentences = pad_sequence(sentences, batch_first=True)
-    sentences = pack_padded_sequence(sentences, lengths_sentence, batch_first=True)
+    chars = pad_sequence(chars,batch_first=True,padding_value=1)
+
+    sentences = pad_sequence(sentences, batch_first=True, padding_value=1)
+    # sentences = pack_padded_sequence(sentences, lengths_sentence, batch_first=True)
 
     labels = pad_sequence(labels, batch_first=True)
-    # labels = pack_padded_sequence(labels, length_label, batch_first=True) # not necessary
 
-    return sentences, labels
+    return (chars, sentences), labels
 
 
 def flatten_sentence(tokens):
@@ -122,17 +123,34 @@ class CONLLDataset(Dataset.Dataset):
         tokens_char = flatten_char(chars)
         tokens_label = flatten_sentence(labels)
 
-        self.char_vocab = Vocab(tokens_char, min_freq=5) if char_vocab is None else char_vocab
-        self.sentence_vocab = Vocab(tokens_sentence, min_freq=5) if sentence_vocab is None else sentence_vocab
+        self.char_vocab = Vocab(tokens_char, min_freq=5,
+                                reserved_tokens=['<pad>']) if char_vocab is None else char_vocab
+        self.sentence_vocab = Vocab(tokens_sentence, min_freq=5,
+                                    reserved_tokens=['<pad>']) if sentence_vocab is None else sentence_vocab
         self.label_vocab = Vocab(tokens_label, has_unk=False) if label_vocab is None else label_vocab
 
         self.chars = []
+        size_chars = []
         for sent in chars:
+            temp_token = []
+            temp_len = []
+            for word in sent:
+                token = torch.LongTensor(self.char_vocab[word])
+                temp_token.append(token)
+                temp_len.append(token.size(0))
+            self.chars.append(temp_token)
+            size_chars.append(max(temp_len))
+        max_chars = max(size_chars)
+        padded_chars = []
+        for sent in self.chars:
             temp = []
             for word in sent:
-                temp.append(torch.LongTensor(self.char_vocab[word]))
-            self.chars.append(temp)
+                word = truncate_pad(list(word), max_chars, self.char_vocab['<pad>'])
+                temp.append(word)
+            temp = [t.tolist() for t in temp]
+            padded_chars.append(torch.LongTensor(temp))
 
+        self.chars = padded_chars
         self.sentences = [torch.LongTensor(self.sentence_vocab[token]) for token in sentences]
         self.labels = [torch.LongTensor(self.label_vocab[token]) for token in labels]
 
@@ -150,8 +168,8 @@ if __name__ == '__main__':
     char_token = flatten_char(chars)
 
     train_set = CONLLDataset(sentences, labels)
-    pass
 
-    # train_loader = DataLoader(train_set, batch_size=32, shuffle=True, collate_fn=collate_fn)
-    # for X, y in train_loader:
-    #     pass
+    train_loader = DataLoader(train_set, batch_size=32, shuffle=True, collate_fn=collate_fn)
+
+    for (C, S), y in train_loader:
+        pass
--- public/misc.py
+++ public/misc.py
@@ -13,7 +13,7 @@ def count_corpus(tokens):
 def truncate_pad(sentence_idx, length, pad_idx):
     while len(sentence_idx) < length:
         sentence_idx.append(pad_idx)
-    return torch.Tensor(sentence_idx[:length])
+    return torch.LongTensor(sentence_idx[:length])
 
 class Vocab:
     def __init__(self, tokens=None, min_freq=0, reserved_tokens=None, has_unk=True):
